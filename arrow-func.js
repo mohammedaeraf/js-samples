d172b6d9d8303d432baf78281e9d50294469ab9b
Mohammed Aeraf <aeraf@outlook.com>
2025-01-01 12:04:27 +0530

    arrow functions demos

--- arrow-func.js
+++ arrow-func.js
@@ -1,20 +1,24 @@
-// normal function
+// normal function to calculate square
 function square(n) {
   return n * n;
 }
 
+// arrow function to calculate square
 let squareA = n => n * n;
+console.log(squareA(5));  // calling arrow function
 
-// console.log(squareA(5));
-// console.log(squareA(5));
 
+
+// arrow function to calculate area of a rectangle
 let areaRect = (l, b) => {
     console.log(`Area of rectange (${l}, ${b}) is ${l * b}`);
 }
-areaRect(10,20)
+
+areaRect(10,20); // calling arrow function
     
 
 
+// arrow function to calculate square
 let calcFact = (n) => {
   let fact = 1;
   for (let i = 2; i <= n; i++) {
@@ -23,7 +27,7 @@ let calcFact = (n) => {
   console.log(`Factorial of ${n} = ${fact}`);
 };
 
-// calcFact(5);
+calcFact(5); // calling arrow function
 
 
 
--- function-greet.js
+++ function-greet.js
@@ -1,7 +1,6 @@
-
 function greet(name) {
     console.log(`Hello ${name}`);
     console.log('Have a wonderful day programming!');
 }
 
-greet(3);
+greet('John');

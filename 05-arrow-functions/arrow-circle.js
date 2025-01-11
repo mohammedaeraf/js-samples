b1817c9ca6e730b90513ddfc646fbb9dda1fb4e0
Aeraf <aeraf@outlook.com>
2025-01-11 10:01:28 +0530

    updated

--- 05-arrow-functions/arrow-circle.js
+++ 05-arrow-functions/arrow-circle.js
@@ -1,5 +1,5 @@
-let areaRect = (r) => {
+let areaCircle = (r) => {
     const area = 3.142 * r * r;
     console.log(`Area of Circle of radius ${r} cm is ${area} sq.cm`);
 }
-areaRect(100); 
+areaCircle(100); 

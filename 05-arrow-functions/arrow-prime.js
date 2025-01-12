e05a2cbb1ecdba0fe70bcb2e2e03e7045600f4c0
Aeraf <aeraf@outlook.com>
2025-01-12 09:47:55 +0530

    Explanation for Fibonacci Program

--- 05-arrow-functions/arrow-prime.js
+++ 05-arrow-functions/arrow-prime.js
@@ -2,7 +2,10 @@ let isPrime = (n) => {
   let prime = true; // let's assume that number is prime
 
   for (let i = 2; i < n; i++) {
+    let remainder = n % i;
+    console.log(`Dividing ${n} by ${i} gives remainder ${remainder}`);
     if (n % i == 0) {
+      console.log(`Remainder is 0, so ${n} is not prime.. breaking the loop`);
       prime = false; // number is not prime
       break;
     }
@@ -10,5 +13,4 @@ let isPrime = (n) => {
   prime == true ? console.log("prime") : console.log("not prime");
 };
 
-isPrime(13);  //output: prime
-isPrime(15);  //output: not prime
+isPrime(23);  //output: not prime

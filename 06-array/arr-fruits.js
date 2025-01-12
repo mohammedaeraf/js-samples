8360faae4411bfcf7f2d72e1564479d42fbc648e
Aeraf <aeraf@outlook.com>
2025-01-12 10:01:56 +0530

    explained arr-fruits

--- 06-array/arr-fruits.js
+++ 06-array/arr-fruits.js
@@ -1,12 +1,19 @@
-let fruits = ['apple', 'banana', 'mango', 'orange', 'grape', 'strawberry', 'watermelon', 'pineapple', 'peach', 'kiwi'];
-
+// Output the entire fruits array to the console
 console.log(fruits);
-// console.log(fruits[0]);
 
-// let fruitsCount = fruits.length;
-// console.log(fruitsCount);
-// console.log('******* Fruits ********')
-// for (let i = 0; i < fruits.length; i++) {
-//     console.log(`Fruit #${i+1} - ${fruits[i]}`);
-    
-// }
+// Output the first fruit in the array to the console
+console.log(fruits[0]);
+
+// Get the number of fruits in the array
+let fruitsCount = fruits.length;
+
+// Output the number of fruits to the console
+console.log(fruitsCount);
+
+// Output a header for the fruits list
+console.log('******* Fruits ********');
+
+// Loop through each fruit in the array and output its index and value
+for (let i = 0; i < fruits.length; i++) {
+    console.log(`Fruit #${i+1} - ${fruits[i]}`);    
+}

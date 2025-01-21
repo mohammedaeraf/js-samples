9d8b27b4fce694c3b93cc07d1785c300740ff0b4
Mohammed Aeraf <aeraf@outlook.com>
2025-01-21 12:26:43 +0530

    String Functions

---
+++ 08-string/string-functions.js
@@ -0,0 +1,12 @@
+let firstName = "John";
+console.log(firstName.length);
+console.log(firstName.charAt(0));
+console.log(firstName.substring(0,2));
+console.log(firstName.slice(0,2));
+console.log(firstName.replace('J','D'));
+console.log(firstName.startsWith('j'));
+console.log(firstName.endsWith('n'));
+console.log(firstName.toLowerCase());
+console.log(firstName.toUpperCase());
+
+
